f43fca1fa478c548f375c67acbcc8b04edec0ff9
Joshua Blewitt <joshblewitt@protonmail.com>
2025-07-31 18:40:21 +0100

    Next commit

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Just practicing JS from the following YouTube video by freeCodeCamp:
 https://www.youtube.com/watch?v=PkZNo7MFNFg&list=WL&index=11
 
-Timestamp: 01:02:41
-https://youtu.be/PkZNo7MFNFg?t=3761
+Timestamp: 01:32:23
+https://youtu.be/PkZNo7MFNFg?t=5543
--- script.js
+++ script.js
@@ -272,3 +272,216 @@ function myOutfit() {
 
 console.log(myOutfit()); // Sweater
 console.log(outerWear); // T-shirt
+
+function minusSeven(num) {
+  return num - 7;
+}
+
+console.log(minusSeven(10));
+
+let functionValue = minusSeven(17);
+
+function nextInLine(arr, item) {
+  arr.push(item);
+  return arr.shift();
+}
+
+let testArr = [1, 2, 3, 4, 5];
+
+console.log(`Before: ${JSON.stringify(testArr)}`);
+console.log(nextInLine(testArr, 6));
+console.log(`After: ${JSON.stringify(testArr)}`);
+
+// boolean values
+
+function welcomeToBooleans() {
+  return true; //or false
+}
+
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+console.log(ourTrueOrFalse(true));
+
+// comparison
+
+function testEqual(val) {
+  if (val === 12) {
+    return "Equal";
+  }
+  return "Not equal";
+}
+
+console.log(testEqual("12"));
+
+/*
+strict equality operator
+doesn't do type conversion
+3 === 3 is true
+3 === "3" is false
+*/
+
+function testNotEqual(val) {
+  if (val !== 99) {
+    return "Not equal";
+  }
+  return "Equal";
+}
+
+console.log(testNotEqual("99"));
+
+/* there is the strict inequality operator as well */
+
+function testGreaterThan(val) {
+  if (val > 100) {
+    return "Over 100";
+  }
+
+  if (val > 10) {
+    return "Over 10";
+  }
+
+  return "10 or under";
+}
+
+console.log(testGreaterThan(50));
+
+function testGreaterThanOrEqual(val) {
+  if (val >= 20) {
+    return "20 or over";
+  }
+
+  if (val >= 10) {
+    return "10 or over";
+  }
+
+  return "Less than 10";
+}
+
+console.log(testGreaterThanOrEqual(10));
+
+function testLessThan(val) {
+  if (val < 25) {
+    return "Under 25";
+  }
+
+  if (val < 55) {
+    return "Under 55";
+  }
+
+  return "55 or over";
+}
+
+console.log(testLessThan(10));
+
+function testLessOrEqual(val) {
+  if (val <= 12) {
+    return "Smaller than or euqal to 12";
+  }
+
+  if (val <= 24) {
+    return "Smaller than or equal to 24";
+  }
+
+  return "More than 24";
+}
+
+console.log(testLessOrEqual(10));
+
+function testLogicalAnd(val) {
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+  return "No";
+}
+
+console.log(testLogicalAnd(35));
+
+function testLogicalOr(val) {
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  return "Inside";
+}
+
+console.log(testLogicalOr(15));
+
+function testElse(val) {
+  let result = "";
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or smaller";
+  }
+  return result;
+}
+
+console.log(testElse(10));
+
+// order is very important in else if statements
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+
+console.log(testElseIf(7));
+
+function testSize(num) {
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+}
+
+console.log(testSize(17));
+console.log(testSize(50));
+
+let names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Dobule Bogey",
+  "Go home!",
+];
+
+function golfScore(par, strokes) {
+  if (strokes === 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes === par - 1) {
+    return names[2];
+  } else if (strokes === par) {
+    return names[3];
+  } else if (strokes === par + 1) {
+    return names[4];
+  } else if (strokes === par + 2) {
+    return names[5];
+  } else if (strokes >= par + 3) {
+    return names[6];
+  }
+}
+
+console.log(golfScore(5, 4));
+console.log(golfScore(5, 2));
+console.log(golfScore(5, 8));
